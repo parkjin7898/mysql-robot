9af5d43e40a895fc621d4703d968f1fd6bbc40f9
parkj <parkjin7898@naver.com>
2025-05-27 14:21:07 +0900

    데이터베이스 연결 성공

---
+++ src/main/kotlin/kr/ac/kumoh/s20220499/mysql_robot/MysqlRobotApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20220499.mysql_robot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MysqlRobotApplication
+
+fun main(args: Array<String>) {
+	runApplication<MysqlRobotApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20220499/mysql_robot/MysqlRobotApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20220499.mysql_robot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MysqlRobotApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
